d378ed37d83cece6f0f62c07c248d916579bc55f
João Henriques <joaoh88@gmail.com>
2013-12-31 18:41:22 +0000

    branch change

--- Cmder.bat
+++ Cmder.bat
@@ -1,3 +1,4 @@
 @echo off
 SET CMDER_ROOT=%~dp0
-start %CMDER_ROOT%/vendor/conemu-maximus5/ConEmu64.exe /Icon "%CMDER_ROOT%\icons\cmder.ico" /Title Cmder /LoadCfgFile "%CMDER_ROOT%\config\ConEmu.xml" /cmd cmd /k "%CMDER_ROOT%\vendor\init.bat" "-new_console:d:%USERPROFILE%"
+
+start %CMDER_ROOT%vendor\conemu-maximus5\ConEmu64.exe /Icon "%CMDER_ROOT%icons\cmder.ico" /Title Cmder /LoadCfgFile "%CMDER_ROOT%config\ConEmu.xml" /cmd cmd /k "%CMDER_ROOT%vendor\init.bat" "-new_console:d:%USERPROFILE%"
